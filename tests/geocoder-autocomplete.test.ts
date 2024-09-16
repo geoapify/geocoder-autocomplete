ec30f250ba1427f14b32efa28f449fc193f87606
Oleg Hovsepyan <hovsepyan021@gmail.com>
2024-09-17 02:21:58 +0400

    Adding first tests

--- tests/geocoder-autocomplete.test.ts
+++ tests/geocoder-autocomplete.test.ts
@@ -1,21 +1,68 @@
-import { GeocoderAutocomplete } from "../src";
+import { GeocoderAutocomplete, GeocoderAutocompleteOptions } from "../src";
+
+const options: any = {
+    skipDetails: true,
+    skipIcons: true,
+    placeholder: " ",
+    filter:  {
+        byCircle: {
+            lon: 10,
+            lat: 20,
+            radiusMeters: 30
+        },
+        byCountryCode: ['am'],
+        byRect: {
+            lon1: 10,
+            lat1: 20,
+            lon2: 30,
+            lat2: 40
+        },
+        customFilter: 'example string filter'
+    },
+    bias:  {
+        byCircle: {
+            lon: 10,
+            lat: 20,
+            radiusMeters: 30
+        },
+        byCountryCode: ['am'],
+        byRect: {
+            lon1: 10,
+            lat1: 20,
+            lon2: 30,
+            lat2: 40
+        },
+        byProximity: {
+            lon: 10,
+            lat: 20
+        },
+    },
+    countryCodes: ['ad'],
+    position: {
+        lon: 10,
+        lat: 20
+    }
+};
+
+function createGeocoderAutocomplete() {
+    const myAPIKey = "XXXXX";
+    const inputElement = document.createElement('input');
+    return new GeocoderAutocomplete(
+        inputElement,
+        myAPIKey, options);
+}
 
 describe('GeocoderAutocomplete', () => {
-    let autocomplete: GeocoderAutocomplete;
+    let autocomplete = createGeocoderAutocomplete();
 
-    beforeEach(() => {
-        const myAPIKey = "XXXXX";
-        const inputElement = document.createElement('input');
-        autocomplete = new GeocoderAutocomplete(
-            inputElement,
-            myAPIKey, {
-                skipDetails: true,
-                skipIcons: true,
-                placeholder: " "
-            });
-    });
-
-    it('should initialize without errors', () => {
+    it('should init the options properly', () => {
         expect(autocomplete).toBeDefined();
+        let initiatedOptions: GeocoderAutocompleteOptions = getPrivateProperty(autocomplete, "options")
+        expect(initiatedOptions.filter).toBe(options.filter);
+        expect(initiatedOptions.bias).toBe(options.bias);
     });
 });
+
+function getPrivateProperty(object: any, field: any) {
+    return object[field];
+}
