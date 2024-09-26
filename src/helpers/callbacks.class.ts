b6df01e81b849707dae38ef774f02db4265cfe04
Oleg Hovsepyan <hovsepyan021@gmail.com>
2024-09-27 00:29:28 +0400

    Code refactoring

---
+++ src/helpers/callbacks.class.ts
@@ -0,0 +1,81 @@
+export class Callbacks {
+    public changeCallbacks: ((selectedOption: any) => void)[] = [];
+    public suggestionsChangeCallbacks: ((options: any[]) => void)[] = [];
+    public inputCallbacks: ((input: string) => void)[] = [];
+    public openCallbacks: ((opened: boolean) => void)[] = [];
+    public closeCallbacks: ((opened: boolean) => void)[] = [];
+
+    addCallback(operation: 'select' | 'suggestions' | 'input' | 'close' | 'open', callback: (param: any) => void) {
+        let currentCallbacks = this.getCallbacksByOperation(operation);
+        if(currentCallbacks) {
+            if (currentCallbacks.indexOf(callback) < 0) {
+                currentCallbacks.push(callback);
+            }
+        }
+    }
+
+    removeCallback(operation: 'select' | 'suggestions' | 'input' | 'close' | 'open', callback?: (param: any) => any) {
+        let currentCallbacks = this.getCallbacksByOperation(operation);
+        if(currentCallbacks) {
+            if (currentCallbacks.indexOf(callback) >= 0) {
+                currentCallbacks.splice(currentCallbacks.indexOf(callback), 1);
+                this.setCallbacksByOperation(operation, currentCallbacks);
+            } else if (!callback) {
+                this.setCallbacksByOperation(operation, []);
+            }
+        }
+    }
+
+    private getCallbacksByOperation(operation: "select" | "suggestions" | "input" | "close" | "open") {
+        let currentCallbacks = null;
+        switch (operation) {
+            case 'select': {
+                currentCallbacks = this.changeCallbacks;
+                break;
+            }
+            case 'suggestions': {
+                currentCallbacks = this.suggestionsChangeCallbacks;
+                break;
+            }
+            case 'input': {
+                currentCallbacks = this.inputCallbacks;
+                break;
+            }
+            case 'close': {
+                currentCallbacks = this.closeCallbacks;
+                break;
+            }
+            case 'open': {
+                currentCallbacks = this.openCallbacks;
+                break;
+            }
+        }
+        return currentCallbacks;
+    }
+
+    private setCallbacksByOperation(operation: "select" | "suggestions" | "input" | "close" | "open",
+                                    callbacks: ((data: any) => void)[]) {
+        switch (operation) {
+            case 'select': {
+                this.changeCallbacks = callbacks;
+                break;
+            }
+            case 'suggestions': {
+                this.suggestionsChangeCallbacks = callbacks;
+                break;
+            }
+            case 'input': {
+                this.inputCallbacks = callbacks;
+                break;
+            }
+            case 'close': {
+                this.closeCallbacks = callbacks;
+                break;
+            }
+            case 'open': {
+                this.openCallbacks = callbacks;
+                break;
+            }
+        }
+    }
+}
---
+++ src/helpers/constants.ts
@@ -0,0 +1,7 @@
+export const BY_COUNTRYCODE = 'countrycode';
+export const BY_RECT = 'rect';
+export const BY_CIRCLE = 'circle';
+export const BY_PROXIMITY = 'proximity';
+export const BY_PLACE = 'place';
+export const GEOCODER_URL = "https://api.geoapify.com/v1/geocode/autocomplete";
+export const PLACE_DETAILS_URL = "https://api.geoapify.com/v2/place-details";
